b32406eb0cdd6c4bf8392a491e92cdfbf30ed57a
joy <466427193@qq.com>
2019-03-04 15:06:10 +0800

    add leader and recommend widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,23 +34,28 @@ class _HomePageState extends State<HomePage> {
 
               String leaderImage = data['data']['shopInfo']['leaderImage'];
               String leaderPhone = data['data']['shopInfo']['leaderPhone'];
-
-              return Column(
-                children: <Widget>[
-                  MySwiper(
-                    swiperDataList: swiperDataList,
-                  ),
-                  TopNavigator(
-                    navigatorList: navigatorList,
-                  ),
-                  ADBanner(
-                    advertesPicture: advertesPicture,
-                  ),
-                  LeaderPhone(
-                    leaderImage: leaderImage,
-                    leaderPhone: leaderPhone,
-                  )
-                ],
+              
+              List<Map> recommendlist= (data['data']['recommend'] as List).cast();
+              return SingleChildScrollView(
+          
+                child: Column(
+                  children: <Widget>[
+                    MySwiper(
+                      swiperDataList: swiperDataList,
+                    ),
+                    TopNavigator(
+                      navigatorList: navigatorList,
+                    ),
+                    ADBanner(
+                      advertesPicture: advertesPicture,
+                    ),
+                    LeaderPhone(
+                      leaderImage: leaderImage,
+                      leaderPhone: leaderPhone,
+                    ),
+                    Recommend(recommendList: recommendlist,)
+                  ],
+                ),
               );
             } else {
               return Center(
@@ -164,5 +169,82 @@ class LeaderPhone extends StatelessWidget {
     } else {
       throw 'Could notlanuch $url';
     }
+  }
+}
+
+class Recommend extends StatelessWidget {
+  final List recommendList;
+
+  Recommend({Key key, this.recommendList}) : super(key: key);
+
+  Widget _titleWidget() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10.0, 2.0, 0, 5.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Colors.black12),
+        ),
+      ),
+      child: Text(
+        "商品推荐",
+        style: TextStyle(color: Colors.pink),
+      ),
+    );
+  }
+
+  Widget _item(index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(333),
+        width: ScreenUtil().setWidth(250),
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border:
+                Border(bottom: BorderSide(width: 0.5, color: Colors.black12))),
+        child: Column(
+          children: <Widget>[
+            Image.network(recommendList[index]['image']),
+            Text("¥${recommendList[index]['mallPrice']}"),
+            Text(
+              "¥${recommendList[index]['price']}",
+              style: TextStyle(
+                  decoration: TextDecoration.lineThrough, color: Colors.grey),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _recommendList(){
+      return Container(
+        height: ScreenUtil().setHeight(333),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: recommendList.length,
+          itemBuilder: (context,index){
+            return _item(index);
+          },
+        ),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      // height: ScreenUtil().setHeight(380),
+      margin: EdgeInsets.only(top: 10.0),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommendList()
+        ],
+      ),
+    );
   }
 }
